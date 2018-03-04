a640833287b9f1752aae912ac76241d973930b2b
Ben Andrews <ben13100@msn.com>
2018-03-03 20:27:20 -0600

    Added evalHorz implementation. It (probably) determines the score of a space if only one player has horizontal pieces.

--- cpp/src/minimax_player.cpp
+++ cpp/src/minimax_player.cpp
@@ -94,10 +94,13 @@ int MinimaxPlayer::evaluateBoard(gameboard *board)
 			pos.col = col_index;
 			pos.row = row_index;
 
-			printf("evalVert\n");
-			evalVert(board, &pos);
+			printf("evalVert ");
+            printf("vert score: %d\n", evalVert(board, &pos));
 
+            printf("evalHorz \n");
+            printf("horz score: %d\n", evalHorz(board, &pos));
 
+			printf("\n");
 		}
 
 
@@ -124,7 +127,7 @@ int MinimaxPlayer::evalVert(gameboard *board, Position *pos)
 	int consecutive = 0;
 
 	// if the pieces below our us, add score, else subtract
-	if((*board)[pos->col][check_row -1] == player_number)
+	if((*board)[pos->col][check_row] == player_number)
 		while(check_row >= 0 && (*board)[pos->col][check_row] == player_number)
 		{
 			++consecutive;
@@ -143,8 +146,96 @@ int MinimaxPlayer::evalVert(gameboard *board, Position *pos)
 	if( (consecutive <= 1 && consecutive >= -1) || (consecutive == 2 && pos->row > ROWS -2))
 		return 0;
 
-	printf("vert score: %d\n", consecutive);
 	return consecutive;
+}
+
+
+/*
+ * Find and return the horizontal score for position pos
+ */
+int MinimaxPlayer::evalHorz(gameboard *board, Position *pos)
+{
+	int left_scoring_player = -1;
+    int left_consecutive = 0;
+	int right_scoring_player = -1;
+    int right_consecutive =  0;
+    int check_left_col = pos->col;
+	int check_right_col = pos->col;
+
+    // count any pieces to the left first
+
+	// Ugly incoming
+	//  if there is no space to the left, or
+	//  if there is no piece to the left goto scoring the right side
+	//  reset left check and check the right side
+	// scoring_player gets assigned to the space on the left if the space exists
+    if(check_left_col == 0 || (left_scoring_player = (*board)[check_left_col -1][pos->row]) == BLANK_SPACE)
+	    goto score_right;
+
+    while(--check_left_col >= 0 && (*board)[check_left_col][pos->row] == left_scoring_player)
+        ++left_consecutive;
+
+
+    // count the score to the right
+	//  unfortunately very similar to count left
+	//  if you, dear reader, can think of a way to do both the left and right scores together without
+	//  repeating code and without hurting performance that would be lovely
+	//  until then, forgive us our code duplication
+score_right:
+	// Symmetric ugly
+	//  if there is no space to the right, or
+	//  if there is no piece to the right goto scoring the right side
+	//  reset right check and calculate the total score
+	// scoring_player gets assigned to the space on the right if the space exists
+	if(check_right_col == COLUMNS -1 || (right_scoring_player = (*board)[check_right_col +1][pos->row]) == BLANK_SPACE)
+		goto calc_score;
+
+    while(++check_right_col <= COLUMNS -1 && (*board)[check_right_col][pos->row] == right_scoring_player)
+        ++right_consecutive;
+
+
+calc_score:
+	// if no one scored
+	if(left_consecutive + right_consecutive <= 1)
+		return 0;
+
+	int score = 0;
+	int left_pot = 0;
+	int right_pot = 0;
+
+	// find the potential for the left and right consecutive pieces
+	//   how many blank spaces there are after the consecutive pieces
+	for(int i = 0; i < 3 - left_consecutive; ++i)
+		if(check_left_col >= 0 && (*board)[check_left_col--][pos->row] == BLANK_SPACE)
+			++left_pot;
+		else
+			break;
+
+	for(int i = 0; i < 3 - right_consecutive; ++i)
+		if(check_right_col <= COLUMNS -1 && (*board)[check_right_col++][pos->row] == BLANK_SPACE)
+			++right_pot;
+		else
+			break;
+
+	printf("left  pot: %d\n", left_pot);
+	printf("right pot: %d\n", right_pot);
+
+	// if only one player has consecutive pieces
+	if(left_scoring_player == right_scoring_player
+	|| left_consecutive == 0 || right_consecutive == 0)
+	{
+		// if there consecutive pieces can become a winning sequence
+		score = left_consecutive + right_consecutive;
+		if(score > 1 && score + left_pot + right_pot >= 4)
+			// if the pieces aren't ours, negate the score
+			if(left_scoring_player != player_number || right_scoring_player != player_number)
+				score *= -1;
+	}
+
+	// find the score if both players have consecutive pieces
+
+
+	return score;
 }
 
 
